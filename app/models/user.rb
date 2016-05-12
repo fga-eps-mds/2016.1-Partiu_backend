9fde87b818f592b612f4fffd0fd42c244e9f0214
DylanGuedes <djmgguedes@gmail.com>
2016-05-12 10:03:42 -0300

    Add ride controller module to the coverage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_one :driver, dependent: :destroy
   has_one :passenger, dependent: :destroy
-  # valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, :format => { with: valid_email_regex }
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, :format => { with: valid_email_regex }
+  validates :email, :uniqueness => true
 end
