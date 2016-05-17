1df9e839cde20c11188d70c2af15912eab91db6c
Eduardo Vital <vital-edu@users.noreply.github.com>
2016-05-17 10:24:04 -0300

    Configurações do Projeto (#3)
    - Removed unnused test folder
    - Added project configurations files
    - Added RVM config files
    - Added CodeClimate configuration
    - Added coverage folder to gitignore
    - Configured production environment
    
    Now just the production environment uses PostgreSQL.
    It is not necessary anymore make any configuration in production server.
    
    - Updated README with important information

--- Gemfile
+++ Gemfile
@@ -2,15 +2,12 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.2'
-# Use postgresql as the database for Active Record
-gem 'pg', '~> 0.15'
 # Connection different domain between back-end rails and front-end ionic
 gem 'rack-cors', :require => 'rack/cors'
 # A gem for delegate attributes and associations of belongs_to and has_one!
 gem 'delegate_associations'
 # Brings convention over configuration to your JSON generation.
 gem 'active_model_serializers'
-
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -25,17 +22,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-# Use Unicorn as the app server
-# gem 'unicorn'
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
-
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'sqlite3'
   gem 'byebug'
   gem 'rspec-rails'
   gem 'factory_girl_rails'
@@ -48,4 +37,9 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+end
+
+group :production do
+  gem 'unicorn'
+  gem 'pg'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20160507133025) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "drivers", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "drivers", ["user_id"], name: "index_drivers_on_user_id", using: :btree
+  add_index "drivers", ["user_id"], name: "index_drivers_on_user_id"
 
   create_table "passengers", force: :cascade do |t|
     t.integer  "user_id"
@@ -30,15 +27,15 @@ ActiveRecord::Schema.define(version: 20160507133025) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "passengers", ["user_id"], name: "index_passengers_on_user_id", using: :btree
+  add_index "passengers", ["user_id"], name: "index_passengers_on_user_id"
 
   create_table "passengers_rides", id: false, force: :cascade do |t|
     t.integer "ride_id"
     t.integer "passenger_id"
   end
 
-  add_index "passengers_rides", ["passenger_id"], name: "index_passengers_rides_on_passenger_id", using: :btree
-  add_index "passengers_rides", ["ride_id"], name: "index_passengers_rides_on_ride_id", using: :btree
+  add_index "passengers_rides", ["passenger_id"], name: "index_passengers_rides_on_passenger_id"
+  add_index "passengers_rides", ["ride_id"], name: "index_passengers_rides_on_ride_id"
 
   create_table "rides", force: :cascade do |t|
     t.string   "title"
@@ -57,8 +54,8 @@ ActiveRecord::Schema.define(version: 20160507133025) do
     t.datetime "updated_at",               null: false
   end
 
-  add_index "rides", ["driver_id"], name: "index_rides_on_driver_id", using: :btree
-  add_index "rides", ["vehicle_id"], name: "index_rides_on_vehicle_id", using: :btree
+  add_index "rides", ["driver_id"], name: "index_rides_on_driver_id"
+  add_index "rides", ["vehicle_id"], name: "index_rides_on_vehicle_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -81,8 +78,6 @@ ActiveRecord::Schema.define(version: 20160507133025) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "vehicles", ["driver_id"], name: "index_vehicles_on_driver_id", using: :btree
+  add_index "vehicles", ["driver_id"], name: "index_vehicles_on_driver_id"
 
-  add_foreign_key "passengers_rides", "passengers"
-  add_foreign_key "passengers_rides", "rides"
 end
