c25c796b617720f2e5c7f5e14722e7ef6837ebe4
Eduardo Vital <vitaldu@gmail.com>
2016-06-21 15:41:35 -0300

    Fixed coverage index generation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'coveralls'
-Coveralls.wear!
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 require 'simplecov'
